b12dbc6b620808426265c7366bbcb31e34507281
储文青 <571869993@qq.com>
2016-05-26 18:55:35 +0800

    done

--- sift.py
+++ sift.py
@@ -4,33 +4,36 @@
 
 import numpy as np
 import cv2
-
+import os
+import sys
 
 
 # Step 1 - Obtain the set of bags of features.
 
 height = 256
 width = 256
-dictionary_size = 1024
-base_path = "img/all/apple/"
+dictionary_size = 256
+
 feature_path = "feature/sift/"
-im_list_file = open("img/all/apple.txt", 'r')
-im_list = []
-for line in im_list_file:
-    im_list.append(line.strip())
+if os.path.exists(feature_path) == False:
+    os.makedirs(feature_path)
 
-
-sift = cv2.SIFT()
+sift = cv2.SIFT(0,3,0.02,10,1.6)
 bow_train = cv2.BOWKMeansTrainer(dictionary_size)
 
+image_classes = ['apple', 'pottery', 'other', 'glass']
 
-
-for i in range(len(im_list)):
-    img = cv2.imread(base_path + im_list[i])
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = cv2.resize(img, (height, width))
-    kp, des = sift.detectAndCompute(img, None)
-    bow_train.add(des)
+im_list = []
+for image_class in image_classes:
+    im_list_file = open("img/all/" + image_class + "/" + image_class + ".txt", 'r')
+    for line in im_list_file:
+        im_path = line.strip()
+        img = cv2.imread(im_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img = cv2.resize(img, (height, width))
+        kp, des = sift.detectAndCompute(img, None)
+        print len(kp)
+        bow_train.add(des)
 
 
 dictionary = bow_train.cluster()
@@ -44,16 +47,21 @@ matcher = cv2.FlannBasedMatcher(flann_params, {})
 bow_extract = cv2.BOWImgDescriptorExtractor( extract, matcher )
 
 bow_extract.setVocabulary(dictionary)
-
-for i in range(len(im_list)):
-    img = cv2.imread(base_path + im_list[i])
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = cv2.resize(img, (height, width))
-    output_feature = bow_extract.compute(img, detect.detect(img))
-    img_name = im_list[i].split('/')[-1]
-    img_name = img_name.split('.')[0]
-    output_path = feature_path + img_name + ".npy"
-    np.save(output_path, output_feature)
+for image_class in image_classes:
+    im_list_file = open("img/all/" + image_class + "/" + image_class + ".txt", 'r')
+    output_feature_path = feature_path + image_class + "/"
+    if os.path.exists(output_feature_path) == False:
+        os.makedirs(output_feature_path)
+    for line in im_list_file:
+        im_path = line.strip()
+        img = cv2.imread(im_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img = cv2.resize(img, (height, width))
+        output_feature = bow_extract.compute(img, sift.detect(img, None))[0]
+        img_name = im_path.split('/')[-1]
+        img_name = img_name.split('.')[0]
+        output_path = output_feature_path + img_name + ".npy"
+        np.save(output_path, output_feature)
 
 
 
